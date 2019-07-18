3910a11fcaff95fd53391a73589e4fd7827c1519
unknown <unknown@example.com>
2019-07-18 10:22:29 +0800

    1st commit

---
+++ pages/game/game.js
@@ -0,0 +1,88 @@
+Page({
+  //导航栏点击事件
+  home_selected: function () {
+    this.setData({
+      homeIsSelect: true,
+      gameIsSelect: false,
+      scoreIsSelect: false
+    })
+  },
+  game_selected: function () {
+    this.setData({
+      homeIsSelect: false,
+      gameIsSelect: true,
+      scoreIsSelect: false
+    })
+  },
+  score_selected: function () {
+    this.setData({
+      homeIsSelect: false,
+      gameIsSelect: false,
+      scoreIsSelect: true
+    })
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    homeIsSelect: false,
+    gameIsSelect: true,
+    scoreIsSelect: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
